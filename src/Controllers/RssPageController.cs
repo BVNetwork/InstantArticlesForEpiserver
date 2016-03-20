5853fae4b10c7d81e7929968c1cc03a09b7a585b
Pontus Hagman <pontus@bvnetwork.com>
2016-03-20 22:49:50 +0100

    Updated view paths

--- src/Controllers/RssPageController.cs
+++ src/Controllers/RssPageController.cs
@@ -1,8 +1,13 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using BVNetwork.InstantArticles.Models.Interfaces;
 using BVNetwork.InstantArticles.Models.Pages;
 using BVNetwork.InstantArticles.Models.ViewModels;
+using Castle.Core.Internal;
 using EPiServer;
 using EPiServer.Core;
+using EPiServer.ServiceLocation;
 using EPiServer.Shell;
 using EPiServer.Web.Mvc;
 
@@ -23,14 +28,17 @@ namespace BVNetwork.InstantArticles.Controllers
             //.GetResult();
 
             //var articles = SearchClient.Instance.Search<PageData>()
-               
+
             //    .Filter(x => x.MatchTypeHierarchy(typeof(IInstantArticle)))
             //   // .Filter(x => ((IInstantArticle)x.ExcludeFromFacebook).Match(false))
             // .GetContentResult();
             ////model.InstantArticles = articles.Items
             //// .GetResult();
-            //var instantArticles = _contentLoader.GetChildren<InstantArticlePage>(ContentReference.StartPage);
-            //model.InstantArticles = articles.Cast<IInstantArticle>();
+            //var instantArticles = _contentLoader.GetChildren<IInstantArticle>(ContentReference.StartPage);
+            //model.InstantArticles = instantArticles;
+            var allArticles = new List<PageData>();
+            FindAllInstantArticles(allArticles, ContentReference.StartPage);
+            model.InstantArticles = allArticles.Cast<IInstantArticle>();
             return View(Paths.PublicRootPath + "BVNetwork.InstantArticles/Views/RssPage/Index.cshtml", model);
         }
 
@@ -39,5 +47,20 @@ namespace BVNetwork.InstantArticles.Controllers
             _contentLoader = contentLoader;
         }
 
+        private void FindAllInstantArticles(List<PageData> list, ContentReference parentPage)
+        {
+            var loader = ServiceLocator.Current.GetInstance<IContentLoader>();
+            var children = loader.GetChildren<PageData>(parentPage);
+
+            children.ForEach(pg =>
+            {
+                if (pg is IInstantArticle)
+                {
+                    list.Add(pg);
+                }
+
+                FindAllInstantArticles(list, pg.ContentLink);
+            });
+        }
     }
 }
