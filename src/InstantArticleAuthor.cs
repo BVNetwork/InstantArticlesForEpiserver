2dc34cbd49cb40c556115dda57fc597d8ffca336
Pontus Hagman <pontus@bvnetwork.com>
2016-03-22 14:47:58 +0100

    InstantArticleAuthor

---
+++ src/InstantArticleAuthor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BVNetwork.InstantArticles
+{
+    public class InstantArticleAuthor : IInstantArticleAuthor
+    {
+        public string Name { get; set; }
+        public string Bio { get; set; }
+        public string FacebookUsername { get; set; }
+    }
+}
