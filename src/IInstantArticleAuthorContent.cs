967a8b6f117593daa38900ed44bf34a8fc7c7fde
Pontus Hagman <pontus@bvnetwork.com>
2016-03-22 14:43:08 +0100

    IInstantArticleAuthorContent

---
+++ src/IInstantArticleAuthorContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BVNetwork.InstantArticles
+{
+    public interface IInstantArticleAuthorContent
+    {
+        IInstantArticle CreateInstantArticleAuthor();
+    }
+}
