95cf558359774e691603e9fabb72131caf091ccd
Pontus Hagman <pontus@bvnetwork.com>
2016-03-24 16:07:27 +0100

    InstantArticleImageBlock

---
+++ src/TemplateCoordinator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+using EPiServer.Web.Mvc;
+
+namespace BVNetwork.InstantArticles
+{
+    [ServiceConfiguration(typeof(IViewTemplateModelRegistrator))]
+    public class TemplateCoordinator : IViewTemplateModelRegistrator
+    {
+        public const string BlockFolder = "~/modules/BVNetwork.InstantArticles/Views/Shared/Blocks/";
+        
+   
+        /// <summary>
+        /// Registers renderers/templates which are specific for this module
+        /// </summary>
+        public void Register(TemplateModelCollection viewTemplateModelRegistrator)
+        {
+            viewTemplateModelRegistrator.Add(typeof(IInstantArticleImageBlock), new TemplateModel
+            {
+                Name = "InstantArticleImageBlock",
+                Tags = new[] { "InstantArticle" },
+                AvailableWithoutTag = false,
+                Path = BlockPath("InstantArticleImageBlock.cshtml")
+            });
+
+        }
+
+        private static string BlockPath(string fileName)
+        {
+            return string.Format("{0}{1}", BlockFolder, fileName);
+        }
+
+
+    }
+}
